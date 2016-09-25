4e0006a1e11a8e36c4912503fc50dd8f023e5842
home-pc-computer <404842799@qq.com>
2016-09-26 07:50:44 +0800

    add note

---
+++ note.php
@@ -0,0 +1,19 @@
+markdonw + sublime
+【少侠】hm(312380128) 14:34:17
+刚刚查了下，有人推荐markdown+sublime
+【少侠】hm(312380128) 14:34:37
+还有推荐RAYSNOTE和Quiver的
+【师兄】nothing(992349839) 14:34:50
+就写markdown吧
+【师兄】nothing(992349839) 14:35:08
+现在好多博客系统都支持markdown
+【少侠】hm(312380128) 14:41:45
+恩，看下来感觉markdown的确是不错
+908224171(908224171) 15:20:07
+武装者极数码
+捍卫你的梦想，自由你的灵魂。
+https://wuzhuangzhe.taobao.com
+
+
+
+
